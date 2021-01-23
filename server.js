cadad2602fb8e98353af931aff46ffa018a8e75f
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-01-22 20:43:50 -0500

    Fixed back-end issues with server and dependencies

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const path = require('path');
 
 const app = express();
 app.use(cors());
-app.use(bodyParse.json());
+app.use(bodyParser.json());
 
 // Environment Variable Configuration
 const PORT = process.env.PORT || 5000;
-app.listen('port', PORT);
+app.set('port', PORT);
 
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production') 
