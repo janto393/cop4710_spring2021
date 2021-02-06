398dc300e7a808b4424679e58cd96a2b2b1afd44
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-02-06 17:20:30 -0500

    Added documentation dir

---
+++ doc/database/creation_commands/create_meeting_types_table.sql
@@ -0,0 +1,4 @@
+create table Meeting_Types (
+	ID int primary key not null auto_increment,
+  Name varchar(100) not null
+);
---
+++ doc/database/creation_commands/create_user_roles_table.sql
@@ -0,0 +1,4 @@
+create table User_Roles (
+	ID int primary key not null auto_increment,
+  Name varchar(50) not null
+);
