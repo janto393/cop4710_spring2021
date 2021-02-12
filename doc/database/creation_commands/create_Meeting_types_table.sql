5871174eb2a4c6a82a7e21fd9f5f23e82dba2100
TroyMorgan1205 <tmorgan1205@knights.ucf.edu>
2021-02-11 23:03:41 -0500

    created Meeting, RSO, attendess, member groups, roles

---
+++ doc/database/creation_commands/create_Meeting_types_table.sql
@@ -0,0 +1,6 @@
+use event_manager;
+
+create table Meeting_type(
+ID INT primary key not null,
+name varchar(30) not null
+);
---
+++ doc/database/creation_commands/create_roles_table.sql
@@ -0,0 +1,9 @@
+use event_manager;
+
+create table Roles(
+ID INT primary key not null auto_increment,
+name varchar(30)not null,
+level INT not null
+);
+
+
