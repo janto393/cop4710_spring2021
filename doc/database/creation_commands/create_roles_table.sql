fd0f4da4bb6eec79ef0667a28d2f4980a9817802
Jamil Gonzalez <jamil.gonzalez@icloud.com>
2021-02-25 19:42:56 -0500

    Revert "Database creation"

--- doc/database/creation_commands/create_roles_table.sql
+++
@@ -1,9 +0,0 @@
-use event_manager;
-
-create table Roles(
-ID INT primary key not null auto_increment,
-name varchar(30)not null,
-level INT not null
-);
-
-
--- doc/database/creation_commands/create_states_table.sql
+++
@@ -1,7 +0,0 @@
-use event_manager;
-
-create table States(
-ID int primary key not null auto_increment,
-name varchar(50) not null,
-acronym varchar(2) not null
-);
