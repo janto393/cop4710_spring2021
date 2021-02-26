26c64308d95fbf7532baded930940f498d044f87
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-02-25 19:51:23 -0500

    Added database creation command files directly. Original branch was broken with git

---
+++ doc/database/creation_commands/create_meeting_types_table.sql
@@ -0,0 +1,6 @@
+use event_manager;
+
+create table MeetingTypes(
+ID int primary key not null auto_increment,
+name varchar(30)
+);
---
+++ doc/database/creation_commands/create_users_table.sql
@@ -0,0 +1,15 @@
+use event_manager;
+
+create table Users (
+ID int primary key not null auto_increment,
+firstName varchar(50) not null,
+lastName varchar(50) not null,
+password varchar(30) not null,
+email varchar(50) not null,
+universityID int,
+rsoID int,
+role int not null,
+foreign key (universityID) references Universities(ID),
+foreign key (rsoID) references Registered_Student_Organizations(ID),
+foreign key (role) references Roles(ID)
+);
