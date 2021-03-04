e07b3fc12369a2e01630f985d4bc97c833d57b0d
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-03-03 23:29:09 -0500

    set up parser structure. May convert to typescript

---
+++ api/sqlParser.js
@@ -0,0 +1,56 @@
+class sqlParser
+{
+	constructor()
+	{
+		
+	}
+
+	generateQuery()
+	{
+
+	}
+}
+
+class SelectStatement
+{
+
+}
+
+class FromStatement
+{
+
+}
+
+class WhereStatement
+{
+
+}
+
+class JoinStatement
+{
+	static JOIN_TYPES = {
+		INNER: 0,
+		LEFT: 1,
+		RIGHT: 2,
+		OUTTER: 3
+	};
+
+	constructor(tableName)
+}
+
+class OrderStatement
+{
+
+}
+
+class OrderByStatement
+{
+
+}
+
+class GroupByClause
+{
+
+}
+
+export default sqlParser;
