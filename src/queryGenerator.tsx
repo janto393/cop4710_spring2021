e3defed2dc2f0743150e52de879239af0a7fccf3
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-03-07 00:21:07 -0500

    Implemented select and from clause parsing

--- src/queryGenerator.tsx
+++ src/queryGenerator.tsx
@@ -1,7 +1,8 @@
 export interface GeneratedQuery
 {
 	success: Boolean,
-	query: String
+	query: string,
+	error: string
 };
 
 export enum JoinType
@@ -14,34 +15,34 @@ export enum JoinType
 
 export interface SelectStatement
 {
-	columnName: String
+	columnName: string
 };
 
 export interface FromStatement
 {
-	tableName: String
+	tableName: string
 };
 
 export interface WhereStatement
 {
-	criteria: String
+	criteria: string
 };
 
 export interface JoinStatement
 {
-	tableName: String,
+	tableName: string,
 	joinType: JoinType,
-	criteria: String
+	criteria: string
 };
 
 export interface OrderStatement
 {
-	criteria: String
+	criteria: string
 };
 
 export interface GroupByStatement
 {
-	criteria: String
+	criteria: string
 };
 
 export interface QueryGeneratorInput
@@ -56,5 +57,76 @@ export interface QueryGeneratorInput
 
 export function queryGenerator(input: QueryGeneratorInput)
 {
+	let generatedQuery: GeneratedQuery = {
+		success: false,
+		query: "",
+		error: ""
+	};
 
+
+	///////////////////
+	// SELECT CLAUSE //
+	///////////////////
+
+	let selectClause: string = "SELECT ";
+
+	// ensure there is at least 1 select element
+	if (input.selectStatements.length < 1)
+	{
+		generatedQuery.error = "No SELECT elements specified";
+		return generatedQuery;
+	}
+
+	// combine the select elements
+	for (let i: number = 0; i < input.selectStatements.length; i++)
+	{
+		selectClause.concat(input.selectStatements[i].columnName);
+		
+		if (i === input.selectStatements.length - 1)
+		{
+			selectClause.concat("\n");
+		}
+		else
+		{
+			selectClause.concat(", ");
+		}
+	}
+
+	// join the select statement to the query string
+	generatedQuery.query.concat(selectClause);
+
+
+	/////////////////
+	// FROM CLAUSE //
+	/////////////////
+
+	let fromClause: string = "FROM ";
+
+	// ensure there is at least 1 from element
+	if (input.fromStatements.length < 1)
+	{
+		generatedQuery.error = "No FROM elements specified";
+		return generatedQuery;
+	}
+
+	// combine the from elements
+	for (let i: number = 0; i < input.fromStatements.length; i++)
+	{
+		fromClause.concat(input.fromStatements[i].tableName);
+
+		if (i === input.fromStatements.length - 1)
+		{
+			fromClause.concat("\n");
+		}
+		else
+		{
+			fromClause.concat(", ");
+		}
+	}
+
+	// join the from clause to the query string
+	generatedQuery.query.concat(fromClause);
+
+	generatedQuery.success = true;
+	return generatedQuery;
 }
