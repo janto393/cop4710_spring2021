a16ed4ac19f38a5dbc761437302de4360d7026f4
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-03-11 21:45:58 -0500

    Implemented and tested login endpoint

--- doc/database/creation_commands/create_users_table.sql
+++ doc/database/creation_commands/create_users_table.sql
@@ -2,6 +2,7 @@ use event_manager;
 
 create table Users (
 ID int primary key not null auto_increment,
+username varchar(30) not null,
 firstName varchar(50) not null,
 lastName varchar(50) not null,
 password varchar(30) not null,
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,23 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
+import bodyParser from "body-parser";
+import * as dotenv from "dotenv";
+import express from "express";
+
+// Endpoint imports
+import { login } from "./api/login";
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// bodyParser configuration
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.get('/', (_, res) => {
 	res.status(200).send();
 });
+
+app.post("/api/login", login);
 
 app.listen(PORT, () => console.log("Running on port " + PORT));
