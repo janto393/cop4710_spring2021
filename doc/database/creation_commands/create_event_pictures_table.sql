8e4fdef5250b112ecc537bbed5b9fe1a3b46f72a
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-03-25 21:28:05 -0400

    Updated creation commands in documentation to reflect minor changes in tables

--- doc/database/creation_commands/create_event_pictures_table.sql
+++ doc/database/creation_commands/create_event_pictures_table.sql
@@ -4,7 +4,7 @@ create table Event_Pictures(
 ID int primary key not null auto_increment,
 universityID int not null,
 filename varchar(30) not null,
-picture blob not null,
+picture mediumblob not null,
 position int not null,
 
 foreign key (universityID) references Universities(ID)
--- doc/database/creation_commands/create_profile_pictures_table.sql
+++ doc/database/creation_commands/create_profile_pictures_table.sql
@@ -4,7 +4,7 @@ create table Profile_Pictures(
 ID int primary key not null auto_increment,
 userID int not null,
 filename varchar(30) not null,
-picture blob not null,
+picture mediumblob not null,
 
 foreign key (userID) references Users(ID)
 
