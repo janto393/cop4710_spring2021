10beccbbf5e73d42cefeb7ff8cfb4d18938b5873
JonElliot Antognoni <jonantognoni@hotmail.com>
2021-04-19 20:02:21 -0400

    Updated documentation sql scripts

--- doc/database/creation_commands/create_profile_pictures_table.sql
+++ doc/database/creation_commands/create_profile_pictures_table.sql
@@ -3,7 +3,6 @@ use event_manager;
 create table Profile_Pictures(
 ID int primary key not null auto_increment,
 userID int not null,
-filename varchar(30) not null,
 picture mediumblob not null,
 
 foreign key (userID) references Users(ID)
--- doc/database/creation_commands/create_students_table.sql
+++ doc/database/creation_commands/create_students_table.sql
@@ -3,7 +3,7 @@ USE event_manager;
 CREATE TABLE Students (
 	userID INT NOT NULL PRIMARY KEY,
   universityID INT NOT NULL,
-  isApproved BOOLEAN NOT NULL DEFAULT FALSE,
+  isApproved TINYINT NOT NULL DEFAULT 0,
   FOREIGN KEY (userID) REFERENCES Users(ID),
   FOREIGN KEY (universityID) REFERENCES Universities(ID)
 );
